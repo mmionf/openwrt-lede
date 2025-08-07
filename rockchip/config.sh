07dda62fbff6b8aa30f561a0702e44cc8366d4ce
mmionf <86910496+mmionf@users.noreply.github.com>
2025-08-07 14:46:35 +0800

    Update config.sh

--- rockchip/config.sh
+++ rockchip/config.sh
@@ -1,4 +1,5 @@
 pkg_name=xray-core
+ls feeds/helloworld/$pkg_name/
 rm -rf feeds/helloworld/$pkg_name/Makefile
 cd feeds/helloworld/$pkg_name/
 wget https://github.com/immortalwrt/packages/raw/refs/heads/openwrt-24.10/net/xray-core/Makefile
