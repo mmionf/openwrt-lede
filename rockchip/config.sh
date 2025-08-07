bd96d6cb9b6d8179f13bf1f4925f5a65c5d8e92e
mmionf <86910496+mmionf@users.noreply.github.com>
2025-08-08 06:02:35 +0800

    Update config.sh

--- rockchip/config.sh
+++ rockchip/config.sh
@@ -1,4 +1,19 @@
-#pkg_path='feeds/helloworld/v2ray-plugin'
-#rm $pkg_path/Makefile
-#wget https://github.com/immortalwrt/packages/raw/refs/heads/master/net/v2ray-plugin/Makefile \
-# -P $pkg_path/
+# boot init script by openwrt
+cat <<EOF >package/package/base-files/files/etc/uci-defaults/99-init.sh
+uci set network.lan.ipaddr=192.168.1.18
+uci commit
+#
+cat <<EOL >/etc/hotplug.d/iface/12-eth1
+#!/bin/sh
+
+ETHTOOL=/usr/sbin/ethtool
+
+$ETHTOOL -i eth0 | grep r8152 && \
+[ "$ACTION" = "ifup" ] && \
+$ETHTOOL -K eth0 rx off tx off
+
+$ETHTOOL -i eth1 | grep r8152 && \
+[ "$ACTION" = "ifup" ] && \
+$ETHTOOL -K eth1 rx off tx off
+EOL
+EOF
