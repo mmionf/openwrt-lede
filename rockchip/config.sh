59e9029ae3c51ff9b4cd18de0636098235ce9d72
mmionf <86910496+mmionf@users.noreply.github.com>
2025-08-07 19:14:52 +0800

    Update config.sh

--- rockchip/config.sh
+++ rockchip/config.sh
@@ -1,9 +1,4 @@
-pkg_path='feeds/packages/net'
-find $pkg_path/xray-core/ $pkg_path/v2ray-plugin/ \
- $pkg_path/xray-plugin/ -name Makefile -delete
-wget https://github.com/immortalwrt/packages/raw/refs/heads/master/net/xray-plugin/Makefile \
- -P $pkg_path/xray-plugin/
- wget https://github.com/immortalwrt/packages/raw/refs/heads/master/net/v2ray-plugin/Makefile \
-  -P $pkg_path/v2ray-plugin/
-  wget https://github.com/immortalwrt/packages/raw/refs/heads/master/net/xray-core/Makefile \
-   -P $pkg_path/xray-core/
+pkg_path='feeds/helloworld/v2ray-plugin'
+rm $pkg_path/Makefile
+wget https://github.com/immortalwrt/packages/raw/refs/heads/master/net/v2ray-plugin/Makefile \
+ -P $pkg_path/
