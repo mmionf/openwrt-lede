9af2d595a93728d45bc8d4ff1596264b345e9c28
mmionf <86910496+mmionf@users.noreply.github.com>
2025-08-07 21:27:28 +0800

    Update config.sh

--- rockchip/config.sh
+++ rockchip/config.sh
@@ -1,4 +1,4 @@
-pkg_path='feeds/helloworld/v2ray-plugin'
-rm $pkg_path/Makefile
-wget https://github.com/immortalwrt/packages/raw/refs/heads/master/net/v2ray-plugin/Makefile \
+#pkg_path='feeds/helloworld/v2ray-plugin'
+#rm $pkg_path/Makefile
+#wget https://github.com/immortalwrt/packages/raw/refs/heads/master/net/v2ray-plugin/Makefile \
  -P $pkg_path/
