5d5eecd72ee2b1831c222978ec9b7abc2468eff2
mmionf <86910496+mmionf@users.noreply.github.com>
2025-08-17 21:02:32 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -15,3 +15,11 @@ $ETHTOOL -i eth1 | grep r8152 && \
 [ "$ACTION" = "ifup" ] && \
 $ETHTOOL -K eth1 rx off tx off
 EOF
+
+cat <<'EOA' >/etc/opkg/distfeeds.conf
+src/gz 1 http://onecloud.lan/armv8/kmods/6.6.93-1-9e686cc1e0d5129337ca1ca28c2ab984
+src/gz 2 http://onecloud.lan/armv8/packages/
+src/gz 3 http://onecloud.lan/packages/aarch64_generic/base/
+src/gz 4 http://onecloud.lan/packages/aarch64_generic/luci/
+src/gz 5 http://onecloud.lan/packages/aarch64_generic/packages/
+EOA
