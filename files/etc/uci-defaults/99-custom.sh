bd3d46b921914f9d9b9254d91d6865eb7effc56d
mmionf <86910496+mmionf@users.noreply.github.com>
2025-08-17 20:18:58 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,5 +1,17 @@
-uci set network.lan.ipaddr=192.168.1.1
+uci set network.lan.gateway=192.168.1.8
+uci set network.lan.dns=223.5.5.5
 uci commit
-sleep 5s
-passwd -d root
 
+cat <<'EOF' >/etc/hotplug.d/iface/eth
+#!/bin/sh
+
+ETHTOOL=/usr/sbin/ethtool
+
+$ETHTOOL -i eth0 | grep r8152 && \
+[ "$ACTION" = "ifup" ] && \
+$ETHTOOL -K eth0 rx off tx off
+
+$ETHTOOL -i eth1 | grep r8152 && \
+[ "$ACTION" = "ifup" ] && \
+$ETHTOOL -K eth1 rx off tx off
+EOF
