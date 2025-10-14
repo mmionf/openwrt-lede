88216121dd105fcc44f1aff21d11584ddbcd4b13
mmionf <86910496+mmionf@users.noreply.github.com>
2025-10-14 21:40:24 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,5 +1,5 @@
 # uci set network.lan.gateway=192.168.1.8
-# uci set network.@device[0].ports='eth0'
+uci set network.@device[0].ports='eth0'
 uci set network.lan.dns=223.5.5.5
 uci set network.lan.ipaddr=192.168.1.2
 uci commit
