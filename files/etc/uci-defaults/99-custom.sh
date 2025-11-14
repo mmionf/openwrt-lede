6424f6bedd317f3e9a49a9f6f60b5f1c2f774f10
mmionf <86910496+mmionf@users.noreply.github.com>
2025-11-14 23:23:09 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,9 +1,5 @@
 # uci set network.lan.gateway=192.168.1.8
-uci set network.@device[0].ports='eth0'
 uci set network.lan.dns=223.5.5.5
-uci set network.lan.ipaddr=192.168.1.2
+uci set network.lan.ipaddr=192.168.2.1
 uci commit
 
-cat <<'EOF' >/etc/hotplug.d/iface/01eth
-sysctl -w net.ipv6.conf.br-lan.accept_ra=2
-EOF
