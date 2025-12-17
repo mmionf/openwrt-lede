ad2d5872d7edef7efa2cf73aaa75e55e97b3e63e
mmionf <86910496+mmionf@users.noreply.github.com>
2025-12-18 07:43:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,11 +5,11 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon packa
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 clone_directory(){
-git clone --depth=1 -b $2 $1 other
-mv -f $3 $4
+git clone --depth=1 -b $2 $1 other;shift 2
+mv -f $@ package/
 rm -rf other
 }
 
-clone_directory https://github.com/coolsnowwolf/luci openwrt-23.05 other/applications/{luci-app-passwall,luci-app-passwall2} package/
+clone_directory https://github.com/coolsnowwolf/luci openwrt-23.05 other/applications/{luci-app-passwall,luci-app-passwall2}
 mv feeds/helloworld/luci-app-ssr-plus package/
 ./scripts/feeds install -a
